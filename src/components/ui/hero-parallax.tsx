be8be23dd94935bb48bfe0c808887697f246459e
the-jahid <aenor007@gmail.com>
2024-12-06 14:03:44 +0600

    'button_updated'

--- src/components/ui/hero-parallax.tsx
+++ src/components/ui/hero-parallax.tsx
@@ -114,7 +114,7 @@ export const Header = () => {
   return (
 <BackgroundLines>
   <div className="flex flex-col lg:flex-row items-center justify-center">
-  <div className="relative mx-auto py-20 md:py-40 px-4 w-full lg:w-1/2">
+  <div className="relative mx-auto py-20 md:py-40 px-4 w-full lg:w-1/2 z-50">
   <h1 className="text-4xl  lg:text-7xl font-bold text-white text-center lg:text-left">
     Explore <br /> Meet <br /> Commute
   </h1>
@@ -124,17 +124,11 @@ export const Header = () => {
   </h4>
 
  
-  <div className="flex justify-center lg:justify-start mt-6">
-    <Link href={'/events'} className="relative inline-flex items-center justify-center px-6 py-3 overflow-hidden font-medium text-white transition duration-300 ease-out border-2 border-blue-500 rounded-full shadow-md group">
-      <span className="absolute inset-0 flex items-center justify-center w-full h-full text-blue-500 duration-300 -translate-x-full bg-white group-hover:translate-x-0 ease">
-        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h14M12 5l7 7-7 7"></path>
-        </svg>
-      </span>
-      <span className="absolute flex items-center justify-center w-full h-full text-white transition-all duration-300 transform group-hover:translate-x-full ease">
-        Try Out our Beta
-      </span>
-      <span className="relative invisible">Try Out our Beta</span>
+  <div className="flex justify-center lg:justify-start mt-6 ">
+    <Link href={'/events'} className=" px-6 py-3 font-medium text-white  border-2 border-blue-500 rounded-full  ">
+     
+      
+      <span >Try Out our Beta</span>
     </Link>
   </div>
 </div>
